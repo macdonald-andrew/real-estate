0bd9284570d760f9a091315ad539b5a94a73bd2f
Andrew MacDonald <mac087@gmail.com>
2018-11-18 20:48:34 -0800

    Implemented contact section and functionality

--- btre/settings.py
+++ btre/settings.py
@@ -35,13 +35,14 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -136,9 +137,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-#Messages
+# Messages
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'MY GMAIL'
+EMAIL_HOST_PASSWORD = 'MY PASSWORD'
+EMAIL_USE_TLS = True
